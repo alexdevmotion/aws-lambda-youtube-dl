12732b3e91588eac43b44c5fa4df126e6f8fa24a
Alexandru Constantin <alex@devmotion.ro>
2020-07-31 12:17:15 +0300

    ytdl implementation with bonus features: videoid & videourl; path & quality query params

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const stream = require('stream');
-const youtubedl = require('youtube-dl');
+const ytdl = require('ytdl-core');
 const AWS = require('aws-sdk');
 
-exports.handler = function(event, context, cb) {   
-  if (!event.videoUrl) {
-    return cb(new Error('videoUrl missing in event'));
+/**
+ * @param event pathParameters
+ *   videoId: the id of the video; e.g. for https://www.youtube.com/watch?v=egs0XN-xjA0; the video id is egs0XN-xjA0 [pass either this param or videoUrl]
+ *   videoUrl: the full video URL; e.g. https://www.youtube.com/watch?v=egs0XN-xjA0 [pass either this param or videoId]
+ *   path?: optional folder path to upload to in the S3 bucket; e.g. new_videos
+ *   quality?: options are highest/lowest/highestaudio/lowestaudio/highestvideo/lowestvideo; default: highest
+ */
+exports.handler = function(event, context, cb) {
+  const query = event.queryStringParameters;
+  if (!query || (!query.videoId && !query.videoUrl)) {
+    return cb(new Error('You need to pass either the videoId or the videoUrl parameter'));
   }
-  const s3 = new AWS.S3({apiVersion: '2006-03-01'});
+  let url = query.videoUrl;
+  if (query.videoId) {
+    url = `https://www.youtube.com/watch?v=${query.videoId}`;
+  }
+  const videoId = url.substring(url.indexOf('v=') + 2);
+
   const passtrough = new stream.PassThrough();
-  const dl = youtubedl(event.videoUrl, ['--format=best[ext=mp4]'], {maxBuffer: Infinity});
+  const dl = ytdl(url);
   dl.once('error', (err) => {
     cb(err);
   });
-  const key = `${context.awsRequestId}.mp4`;
+  const key = query.path ? `${query.path}/${videoId}.mp4` : `${videoId}.mp4`;
   const upload = new AWS.S3.ManagedUpload({
     params: {
       Bucket: process.env.BUCKET_NAME,
@@ -22,7 +35,7 @@ exports.handler = function(event, context, cb) {
     partSize: 1024 * 1024 * 64 // in bytes
   });
   upload.on('httpUploadProgress', (progress) => {
-    console.log(`[${event.videoUrl}] copying video ...`, progress);
+    console.log(`[${videoId}] copying video ...`, progress);
   });
   upload.send((err) => {
     if (err) {
@@ -30,7 +43,7 @@ exports.handler = function(event, context, cb) {
     } else {
       cb(null, {
         bucketName: process.env.BUCKET_NAME,
-        key,
+        path: key,
         url: `s3://${process.env.BUCKET_NAME}/${key}`
       });
     }
